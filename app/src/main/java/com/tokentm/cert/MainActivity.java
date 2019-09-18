30ff318858c3a9653a43cdcb2d87a968e8e22c5f
NBXXF <2767356582@qq.com>
2019-09-18 20:32:20 +0800

    feat:1.0 整理并集成网络请求

--- app/src/main/java/com/tokentm/cert/MainActivity.java
+++ app/src/main/java/com/tokentm/cert/MainActivity.java
@@ -1,14 +1,18 @@
 package com.tokentm.cert;
 
+import android.app.Activity;
 import android.os.Bundle;
 
-import androidx.appcompat.app.AppCompatActivity;
+import com.tokentm.cert.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
