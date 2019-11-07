14384423132bd59743e23c437010a7f0099d399a
lqx <herolqx@126.com>
2019-11-07 20:43:50 +0800

    feat:增加身份密码弹窗,动态盖章动画

--- tks_components/src/main/java/com/tokentm/sdk/components/identitypwd/UserIdentityPwdInputAlertDialog.java
+++ tks_components/src/main/java/com/tokentm/sdk/components/identitypwd/UserIdentityPwdInputAlertDialog.java
@@ -1,5 +1,9 @@
 package com.tokentm.sdk.components.identitypwd;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
@@ -71,6 +75,61 @@ public class UserIdentityPwdInputAlertDialog extends BaseAlertDialog<String> {
                 forgotIdentityPwd();
             }
         });
+        //延时一段时间在执行动画,因为进来先弹键盘
+        binding.tksComponentsUserDialogDecryptedWithRedChapterIv.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startPropertyAnim();
+            }
+        },800);
+    }
+
+    /**
+     * 动画实际执行
+     */
+    private void startPropertyAnim() {
+        // X轴方向上的坐标
+        float translationX = binding.tksComponentsUserDialogDecryptedByReadChapter.getTranslationX();
+        float translationY = binding.tksComponentsUserDialogDecryptedByReadChapter.getTranslationY();
+
+        //这个还小
+        float left = binding.tksComponentsUserDialogDecryptedWithRedChapterIv.getLeft();
+        float left1 = binding.tksComponentsUserDialogDecryptedByReadChapter.getLeft();
+
+        //这个还小
+        int top = binding.tksComponentsUserDialogDecryptedWithRedChapterIv.getTop();
+        int top1 = binding.tksComponentsUserDialogDecryptedByReadChapter.getTop();
+
+        ObjectAnimator animator = ObjectAnimator.ofFloat(binding.tksComponentsUserDialogDecryptedByReadChapter, "scaleY", 1f, 0.7f, 1f);
+        ObjectAnimator animator1 = ObjectAnimator.ofFloat(binding.tksComponentsUserDialogDecryptedByReadChapter, "scaleX", 1f, 0.7f, 1f);
+        AnimatorSet animSet1 = new AnimatorSet();
+        animSet1.play(animator).with(animator1);
+        animSet1.setDuration(900);
+        animSet1.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                binding.tksComponentsUserDialogDecryptedByReadChapter.setVisibility(View.INVISIBLE);
+                binding.tksComponentsUserDialogDecryptedWithRedChapterIv.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // 向右移动500pix，然后再移动到原来的位置复原。
+        // 参数“translationX”指明在x坐标轴位移，即水平位移。
+        ObjectAnimator anim = ObjectAnimator.ofFloat(binding.tksComponentsUserDialogDecryptedByReadChapter, "translationX", translationX, -(left1-left));
+        ObjectAnimator anim1 = ObjectAnimator.ofFloat(binding.tksComponentsUserDialogDecryptedByReadChapter, "translationY", translationY, -(top1-top));
+
+        AnimatorSet animSet = new AnimatorSet();
+        animSet.play(anim).with(anim1);
+        animSet.setDuration(900);
+        animSet.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                animSet1.start();
+            }
+        });
+        animSet.start();
     }
 
     private void forgotIdentityPwd() {
